647acc24de12c223c34d3ff6242a6b2df8a18120
Laxmi Bharti <167627084+bhartilaxmi273@users.noreply.github.com>
2025-07-12 17:01:35 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://bhartilaxmi273:food123@cluster0.vkpyzjh.mongodb.net/food-del"),(() => console.log("DB connected"));
-    
-}
